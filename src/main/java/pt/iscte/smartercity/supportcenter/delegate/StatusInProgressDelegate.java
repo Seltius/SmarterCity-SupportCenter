56f1a3f8775114b829edc7148e7ee9726fb2502e
Jose Joao Dantas <jose_dantas_80@hotmail.com>
2021-12-11 22:21:16 +0000

    Add StatusInProgressDelegate and refactor SupportRequestDelegate

---
+++ src/main/java/pt/iscte/smartercity/supportcenter/delegate/StatusInProgressDelegate.java
@@ -0,0 +1,27 @@
+package pt.iscte.smartercity.supportcenter.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import pt.iscte.smartercity.supportcenter.service.dto.SupportProcessDTO;
+
+@Component
+public class StatusInProgressDelegate implements JavaDelegate {
+
+    private static final Logger log = LoggerFactory.getLogger(StatusInProgressDelegate.class);
+
+    private static final String IN_PROGRESS = "In Progress";
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        log.info("RUNNING DELEGATE TASK: StatusInProgressDelegate");
+
+        //GET PROCESS INSTANCE
+        SupportProcessDTO supportProcess = (SupportProcessDTO) delegateExecution.getVariable("processInstance");
+
+        // SET STATUS TO IN_PROGRESS
+        supportProcess.getSupport().setStatus(IN_PROGRESS);
+    }
+}
--- src/main/java/pt/iscte/smartercity/supportcenter/delegate/SupportRequestDelegate.java
+++ src/main/java/pt/iscte/smartercity/supportcenter/delegate/SupportRequestDelegate.java
@@ -1,8 +1,11 @@
 package pt.iscte.smartercity.supportcenter.delegate;
 
 import java.time.LocalDate;
+import java.util.Date;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pt.iscte.smartercity.supportcenter.service.SupportService;
@@ -11,19 +14,27 @@ import pt.iscte.smartercity.supportcenter.service.dto.SupportProcessDTO;
 @Component
 public class SupportRequestDelegate implements JavaDelegate {
 
+    private static final Logger log = LoggerFactory.getLogger(SupportRequestDelegate.class);
+
     @Autowired
     SupportService supportService;
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
+        log.info("RUNNING DELEGATE TASK: SupportRequestDelegate");
+
+        //GET PROCESS INSTANCE
         SupportProcessDTO supportProcess = (SupportProcessDTO) delegateExecution.getVariable("processInstance");
 
-        //GENERATE ID //TODO RANDOM ID BASED ON TIMESTAMP
-        supportProcess.getSupport().setSupportId(1);
+        //GENERATE TICKET ID BASED ON TIMESTAMP
+        supportProcess.getSupport().setSupportId(generateUniqueId());
 
-        //GENERATE START DATE
+        //GENERATE START DATE AND PERSIST
         supportProcess.getSupport().setStartDate(LocalDate.now());
-
         supportService.save(supportProcess.getSupport());
+    }
+
+    private Integer generateUniqueId() {
+        return (int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE);
     }
 }
