76c95f65a65a2b3910c644fde1a56f838506d44b
Jose Joao Dantas <jose_dantas_80@hotmail.com>
2021-12-11 21:34:31 +0000

    Add SupportRequestDelegate

---
+++ src/main/java/pt/iscte/smartercity/supportcenter/delegate/SupportRequestDelegate.java
@@ -0,0 +1,29 @@
+package pt.iscte.smartercity.supportcenter.delegate;
+
+import java.time.LocalDate;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import pt.iscte.registration.service.SupportService;
+import pt.iscte.registration.service.dto.SupportProcessDTO;
+
+@Component
+public class SupportRequestDelegate implements JavaDelegate {
+
+    @Autowired
+    SupportService supportService;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        SupportProcessDTO supportProcess = (SupportProcessDTO) delegateExecution.getVariable("processInstance");
+
+        //GENERATE ID //TODO RANDOM ID BASED ON TIMESTAMP
+        supportProcess.getSupport().setSupportId(1);
+
+        //GENERATE START DATE
+        supportProcess.getSupport().setStartDate(LocalDate.now());
+
+        supportService.save(supportProcess.getSupport());
+    }
+}
